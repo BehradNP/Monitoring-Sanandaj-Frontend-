971b7ea584c2cf96962fcc090d22750c1122b097
BehradNP <behradrostami2024@gmail.com>
2026-07-18 18:13:53 +0330

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const INTERNAL_API_URL =
+  process.env.INTERNAL_API_URL || "https://msfmapi.sanandaj.ir/api/v1";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/v1/:path*",
+        destination: `${INTERNAL_API_URL}/:path*`,
+      },
+      {
+        source: "/api-proxy/:path*",
+        destination: `${INTERNAL_API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
